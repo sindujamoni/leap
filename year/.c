12afc0f3ce93d1378668601a5e07f33d3efbf2a1
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-01-08 16:38:05 +0530

    Create .c

---
+++ year/.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int year;
+printf("enter a year");
+scanf("%d,&yaer");
+if(yaer%4==0)
+{
+printf("it is a leap year");
+}
+else
+{
+printf("it is not a leap year");
+}
+return 0;
+}
